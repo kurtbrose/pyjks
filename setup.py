eeeee2a6d3251db9f94a04599e5c02daf0d62948
Kurt Rose <=>
2015-03-03 23:01:49 -0800

    fixing pip install

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 import os
 
-
-readme_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'README.md')
-with open(readme_path) as f:
-    long_description = f.read()
+try:
+    readme_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'README.md')
+    with open(readme_path) as f:
+        long_description = f.read()
+except:
+    long_description = None
 
 setup(
     name='pyjks',
-    version='0.3',
+    version='0.3.0.1',
     author="Kurt Rose",
     author_email="kurt@kurtrose.com",
     description='pure python jks file parser',
