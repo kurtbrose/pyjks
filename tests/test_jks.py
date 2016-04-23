5e03f2b4c3a70d50f06889e2a087f15d5740570f
voetsjoeba <voetsjoeba+github@gmail.com>
2016-04-23 19:41:18 +0200

    tests: Added missing algorithm and size checks for SecretKey tests

--- tests/test_jks.py
+++ tests/test_jks.py
@@ -56,26 +56,36 @@ class JceksTests(AbstractTest):
         store = jks.KeyStore.load("tests/keystores/jceks/DES.jceks", "12345678")
         sk = self.find_secret_key(store, "mykey")
         self.assertEqual(sk.key, "\x4c\xf2\xfe\x91\x5d\x08\x2a\x43")
+        self.assertEqual(sk.algorithm, "DES")
+        self.assertEqual(sk.size, 64)
 
     def test_desede_secret_key(self):
         store = jks.KeyStore.load("tests/keystores/jceks/DESede.jceks", "12345678")
         sk = self.find_secret_key(store, "mykey")
         self.assertEqual(sk.key, "\x67\x5e\x52\x45\xe9\x67\x3b\x4c\x8f\xc1\x94\xce\xec\x43\x3b\x31\x8c\x45\xc2\xe0\x67\x5e\x52\x45")
+        self.assertEqual(sk.algorithm, "DESede")
+        self.assertEqual(sk.size, 192)
 
     def test_aes128_secret_key(self):
         store = jks.KeyStore.load("tests/keystores/jceks/AES128.jceks", "12345678")
         sk = self.find_secret_key(store, "mykey")
         self.assertEqual(sk.key, "\x66\x6e\x02\x21\xcc\x44\xc1\xfc\x4a\xab\xf4\x58\xf9\xdf\xdd\x3c")
+        self.assertEqual(sk.algorithm, "AES")
+        self.assertEqual(sk.size, 128)
 
     def test_aes256_secret_key(self):
         store = jks.KeyStore.load("tests/keystores/jceks/AES256.jceks", "12345678")
         sk = self.find_secret_key(store, "mykey")
         self.assertEqual(sk.key, "\xe7\xd7\xc2\x62\x66\x82\x21\x78\x7b\x6b\x5a\x0f\x68\x77\x12\xfd\xe4\xbe\x52\xe9\xe7\xd7\xc2\x62\x66\x82\x21\x78\x7b\x6b\x5a\x0f")
+        self.assertEqual(sk.algorithm, "AES")
+        self.assertEqual(sk.size, 256)
 
     def test_pbkdf2_hmac_sha1(self):
         store = jks.KeyStore.load("tests/keystores/jceks/PBKDF2WithHmacSHA1.jceks", "12345678")
         sk = self.find_secret_key(store, "mykey")
         self.assertEqual(sk.key, "\x57\x95\x36\xd9\xa2\x7f\x7e\x31\x4e\xf4\xe3\xff\xa5\x76\x26\xef\xe6\x70\xe8\xf4\xd2\x96\xcd\x31\xba\x1a\x82\x7d\x9a\x3b\x1e\xe1")
+        self.assertEqual(sk.algorithm, "PBKDF2WithHmacSHA1")
+        self.assertEqual(sk.size, 256)
 
     def test_rsa_1024(self):
         store = jks.KeyStore.load("tests/keystores/jceks/RSA1024.jceks", "12345678")
