97a1093107748e64f1e199e41d42da0cc4b6c118
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-04-22 22:11:17 -0700

    bump version for 19.0.1dev

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjks',
-    version='19.0.0',
+    version='19.0.1dev',
     author="Kurt Rose, Jeroen De Ridder",
     author_email="kurt@kurtrose.com",
     description='Pure-Python Java Keystore (JKS) library',
